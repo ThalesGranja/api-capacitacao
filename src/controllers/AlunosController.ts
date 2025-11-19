492c29879c70980a1862505a4920f833c4fd62b1
Thales Granja <thalesgranja.dev@gmail.com>
2025-11-19 01:19:25 -0300

    feat: adicionar funcionalidade retornar todos os alunos

--- src/controllers/AlunosController.ts
+++ src/controllers/AlunosController.ts
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { AlunoModel } from "../models/Aluno";
 
 export const AlunoController = {
-  // Criar aluno
+  // criar aluno
   create: async (req: Request, res: Response) => {
     try {
       const { name, age, ra, cpf } = req.body;
@@ -21,6 +21,16 @@ export const AlunoController = {
       });
 
       return res.status(201).json(aluno);
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
+  },
+
+  // Listar todos
+  getAll: async (req: Request, res: Response) => {
+    try {
+      const alunos = await AlunoModel.find();
+      return res.status(200).json(alunos);
     } catch (error) {
       return res.status(500).json({ error });
     }
--- src/routes/alunos.routes.ts
+++ src/routes/alunos.routes.ts
@@ -4,5 +4,6 @@ import { AlunoController } from "../controllers/AlunosController";
 const router = Router();
 
 router.post("/", AlunoController.create);
+router.get("/", AlunoController.getAll);
 
 export default router;
