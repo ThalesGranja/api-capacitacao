d95dc622188a13422a6f7f77bd43c0ba50ef726c
Thales Granja <thalesgranja.dev@gmail.com>
2025-11-19 00:53:16 -0300

    feat: adicionar controller de alunos

---
+++ src/controllers/AlunosController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import dayjs from "dayjs";
+import { AlunoModel } from "../models/Aluno";
+
+export const AlunoController = {
+  // Criar aluno
+  create: async (req: Request, res: Response) => {
+    try {
+      const { name, age, ra, cpf } = req.body;
+
+      const createdAt = dayjs().toDate();
+      const updatedAt = createdAt;
+
+      const aluno = await AlunoModel.create({
+        name,
+        age,
+        ra,
+        cpf,
+        createdAt,
+        updatedAt
+      });
+
+      return res.status(201).json(aluno);
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
+  }
+};
