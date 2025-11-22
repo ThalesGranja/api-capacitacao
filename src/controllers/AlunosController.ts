0d1900f78a3a27c45d5a900c83e6c9d5f5213aba
Thales Granja <thalesgranja.dev@gmail.com>
2025-11-22 19:31:04 -0300

    feat: adicionar delecao de alunos via id

--- src/controllers/AlunosController.ts
+++ src/controllers/AlunosController.ts
@@ -70,6 +70,20 @@ export const AlunoController = {
     } catch (error) {
       return res.status(500).json({ error });
     }
+  },
+
+  // deletar
+  delete: async (req: Request<{ id: string }>, res: Response) => {
+    try {
+      const { id } = req.params;
+
+      const aluno = await AlunoModel.findByIdAndDelete(id);
+      if (!aluno) return res.status(404).json({ message: "Aluno não encontrado" });
+
+      return res.status(200).json({ message: "Aluno deletado com sucesso" });
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
   }
 
 };
--- src/routes/alunos.routes.ts
+++ src/routes/alunos.routes.ts
@@ -7,5 +7,6 @@ router.post("/", AlunoController.create);
 router.get("/", AlunoController.getAll);
 router.get("/:id", AlunoController.getById);
 router.put("/:id", AlunoController.update);
+router.delete("/:id", AlunoController.delete);
 
 export default router;
